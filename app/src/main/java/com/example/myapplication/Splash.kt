b9136cba287bf0437571e925a8c72c5b6352cd80
maria <mariami.baliashvili.1@btu.edu.ge>
2022-01-28 00:13:55 +0400

    B&B

---
+++ app/src/main/java/com/example/myapplication/Splash.kt
@@ -0,0 +1,20 @@
+package com.example.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class Splash : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        val handler = Handler(Looper.getMainLooper())
+        handler.postDelayed({
+            val intent = Intent(this,LoginActivity::class.java)
+            startActivity(intent)
+        },3000)
+    }
+}
